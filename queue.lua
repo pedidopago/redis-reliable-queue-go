dc2c5b07947e277bed69c7205d64c5985b7a684e
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2023-01-27 16:12:50 -0300

    remove unecessary number conversion

--- queue.lua
+++ queue.lua
@@ -1,6 +1,6 @@
 local list_name = KEYS[1]
 local acknowledged_list = KEYS[2]
-local item_expiration_timestamp_str = tonumber(KEYS[3])
+local item_expiration_timestamp_str = KEYS[3]
 local list_expiration_time = tonumber(KEYS[4])
 local pop_command = KEYS[5]
 local push_command = KEYS[6]
